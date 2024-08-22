49c7d9bb12a8b9e859c130a74b430cf1c1884461
NahuelCJuarez <cherai92@gmail.com>
2024-08-22 19:12:22 -0300

    navbar basica

--- front/instituto_murasaki/src/components/Navbar/Navbar.tsx
+++ front/instituto_murasaki/src/components/Navbar/Navbar.tsx
@@ -1,15 +1,25 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import styles from "./navbar.module.css"
+import Logo from "./Murasaki_logo.png"
 
 const Navbar: React.FC = () => {
   return (
-    <nav className={styles.navbar}>
-      <div className={styles.container}>
-        <Link to="/" className={styles.link}>Inicio</Link>
-        <Link to="/login" className={styles.link}>Login</Link>
+    <div className={styles.navbar}>
+      <div className={styles.header}>
+        <div className={styles.left}>
+          <img src={Logo} alt="Logo" />
+          <h1>Instituto Murasaki</h1>
+        </div>
+        <Link to="/login" className={`${styles.link} ${styles.button}`}>Ingresar</Link>
       </div>
-    </nav>
+      <div className={styles.navLinks}>
+        <Link to="/" className={styles.navLink}>INICIO</Link>
+        <Link to="/Eventos" className={styles.navLink}>EVENTOS</Link>
+        <Link to="/Informacion" className={styles.navLink}>INFORMACION</Link>
+        <Link to="/Discord" className={styles.navLink}>DISCORD</Link>
+      </div>
+    </div>
   );
 };
 
